d36f7f5c6b17885d647910dd80c01cf74cb64b7f
Andrej <andrej@git.innernet>
2016-11-28 12:52:56 +0200

    commit

--- Android/HomeWork003/ListView/app/src/main/java/com/example/eugen/listview/MainActivity.java
+++ Android/HomeWork003/ListView/app/src/main/java/com/example/eugen/listview/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.eugen.listview;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,6 +11,8 @@ public class MainActivity extends AppCompatActivity {
 
     private EditText textEditText;
     private ListView itemsListView;
+    private EditText _textEditText;
+    private StringItemArrayAdapter _adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,9 +21,21 @@ public class MainActivity extends AppCompatActivity {
 
         textEditText = (EditText) findViewById(R.id.textEditText);
         itemsListView = (ListView) findViewById(R.id.itemsListView);
+        _textEditText = (EditText) findViewById(R.id.textEditText);
+        _adapter = new StringItemArrayAdapter(this);
+
+        ListView itemsListView = (ListView) findViewById(R.id.itemsListView);
+        itemsListView.setAdapter(_adapter);
     }
 
     public void addItem(View view){
 
+        String text = _textEditText.getText()
+                .toString()
+                .trim();
+
+        if (text.length() > 0){
+            _adapter.add(new StringItem(text));
+        }
     }
 }
