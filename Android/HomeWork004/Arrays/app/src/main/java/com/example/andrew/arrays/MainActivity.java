293eeb03bd6ac401b217e72f584ab76651526347
Andrej <andrej@git.innernet>
2016-12-08 11:42:40 +0200

    added task 002

--- Android/HomeWork004/Arrays/app/src/main/java/com/example/andrew/arrays/MainActivity.java
+++ Android/HomeWork004/Arrays/app/src/main/java/com/example/andrew/arrays/MainActivity.java
@@ -28,38 +28,73 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onCalculateTask1(View view){
-
-        String[] items = arrayAEditText
-                .getText()
-                .toString()
-                .split(",");
-        ArrayList<Integer> negotive = new ArrayList<>();
-        ArrayList<Integer> positive = new ArrayList<>();
-        try{
-            for (String item: items){
-                Integer i = Integer.parseInt(item.trim());
-                if (i < 0){
-                    negotive.add(i);
-                } else {
-                    positive.add(i);
-                }
-            }
-            negotive.addAll(positive);
-
-            Integer[] result = new Integer[negotive.size()];
-            negotive.toArray(result);
-
-            arrayBTextView.setText(toPrintable(result));
-        } catch (Throwable e){
-            arrayBTextView.setText("Error!");
+        Integer[] items = parseArrayA();
+        if(items.length == 0){
+            return;
         }
+
+        ArrayList<Integer> negative = new ArrayList<>();
+        ArrayList<Integer> positive = new ArrayList<>();
+        for (Integer item: items){
+            if (item < 0){
+                negative.add(item);
+            } else {
+                positive.add(item);
+            }
+        }
+        negative.addAll(positive);
+        Integer[] result = new Integer[negative.size()];
+        negative.toArray(result);
+        arrayBTextView.setText(toPrintable(result));
     }
 
+    public void onCalculateTask2(View view){
+        Integer[] items = parseArrayA();
+        if(items.length == 0){
+            return;
+        }
+
+        Integer[] result = new Integer[items.length];
+        for (int i = 0; i < items.length; i++){
+            if (result[i] != null && result[i].equals(1)){
+                continue;
+            }
+            result[i] = 0;
+            for (int j = i + 1; j < items.length; j++){
+                if (items[i].equals(items[j])){
+                    result[i] = 1;
+                    result[j] = 1;
+                }
+            }
+        }
+
+        arrayBTextView.setText(toPrintable(result));
+    }
+
+    private Integer[] parseArrayA(){
+        try {
+            String[] items = arrayAEditText
+                    .getText()
+                    .toString()
+                    .split(",");
+            Integer[] result = new Integer[items.length];
+            for (int i = 0; i < result.length; i++){
+                result[i] = Integer.parseInt(items[i].trim());
+            }
+            return result;
+        } catch (Throwable e){
+            arrayBTextView.setText("Error!");
+            return new Integer[0];
+        }
+    }
     @NonNull
     private String toPrintable(Integer[] array){
-        StringBuilder sb= new StringBuilder();
-        for (Integer i: array){
-            sb.append(i + ", ");
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < array.length; i++){
+            sb.append(array[i].toString());
+            if (i != array.length - 1){
+                sb.append(", ");
+            }
         }
         return sb.toString();
     }
