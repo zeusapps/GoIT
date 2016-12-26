effcb34cf212fb01ec2793a1e4fefb893547fcbe
Andrej <andrej@git.innernet>
2016-12-26 14:21:17 +0200

    added impl for popup and action mode

--- Android/HomeWork009/Homework009/app/src/main/java/com/example/eugen/homework009/MainActivity.java
+++ Android/HomeWork009/Homework009/app/src/main/java/com/example/eugen/homework009/MainActivity.java
@@ -2,16 +2,24 @@ package com.example.eugen.homework009;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.PopupMenu;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.view.ActionMode;
+import android.widget.Toast;
 
 import com.example.eugen.homework009.adapters.ModelAdapter;
 import com.example.eugen.homework009.models.ActionModeCallbackModel;
+import com.example.eugen.homework009.models.ModelBase;
 import com.example.eugen.homework009.models.PopupModel;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener, ActionMode.Callback, PopupMenu.OnMenuItemClickListener {
 
     @BindView(R.id.main_modelItemListView)
     ListView _listView;
@@ -30,5 +38,67 @@ public class MainActivity extends AppCompatActivity {
         _adapter.add(new ActionModeCallbackModel("action mode callback"));
 
         _listView.setAdapter(_adapter);
+
+        _listView.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+        ModelBase item = _adapter.getItem(position);
+        if (item instanceof PopupModel){
+            showPopup(view);
+        }
+
+        if (item instanceof ActionModeCallbackModel){
+            showActionMode();
+        }
+    }
+
+    private void showActionMode() {
+        startActionMode(this);
+    }
+
+    private void showPopup(View view){
+        PopupMenu menu = new PopupMenu(this, view);
+        menu.inflate(R.menu.popup_menu);
+        menu.show();
+
+        menu.setOnMenuItemClickListener(this);
+    }
+
+
+
+
+
+    @Override
+    public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+        mode.getMenuInflater().inflate(R.menu.popup_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+        return false;
+    }
+
+    @Override
+    public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+        showClicked(item.getTitle().toString());
+        return true;
+    }
+
+    @Override
+    public void onDestroyActionMode(ActionMode mode) {
+
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        showClicked(item.getTitle().toString());
+        return true;
+    }
+
+    private void showClicked(String title){
+        Toast.makeText(this, title, Toast.LENGTH_SHORT).show();
     }
 }
--- Android/HomeWork009/Homework009/app/src/main/java/com/example/eugen/homework009/adapters/ModelAdapter.java
+++ Android/HomeWork009/Homework009/app/src/main/java/com/example/eugen/homework009/adapters/ModelAdapter.java
@@ -1,13 +1,11 @@
 package com.example.eugen.homework009.adapters;
 
 import android.content.Context;
-import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.eugen.homework009.R;
@@ -35,11 +33,8 @@ public final class ModelAdapter extends ArrayAdapter<ModelBase> {
 
         ViewHolder holder;
 
-        if (convertView == null){
-
-            convertView =
-                    //((LayoutInflater)_context.getSystemService(Context.LAYOUT_INFLATER_SERVICE))
-                    LayoutInflater
+        if (convertView == null) {
+            convertView = LayoutInflater
                     .from(_context)
                     .inflate(_resource, parent, false);
             holder = new ViewHolder(convertView);
@@ -53,29 +48,26 @@ public final class ModelAdapter extends ArrayAdapter<ModelBase> {
 
     }
 
-
-
-    class ViewHolder{
+    class ViewHolder {
         @BindView(R.id.template_title)
         TextView titleTextView;
-        View _view;
 
-        private ViewHolder(View view){
+        private ViewHolder(View view) {
             ButterKnife.bind(this, view);
-            _view = view;
         }
-        private void update(int position){
+
+        private void update(int position) {
             ModelBase item = getItem(position);
 
             if (item != null) {
                 titleTextView.setText(item.getTitle());
 
-                if (item instanceof PopupModel){
+                if (item instanceof PopupModel) {
                     titleTextView.setBackgroundColor(
                             _context.getResources().getColor(R.color.popupModel));
                 }
 
-                if (item instanceof ActionModeCallbackModel){
+                if (item instanceof ActionModeCallbackModel) {
                     titleTextView.setBackgroundColor(
                             _context.getResources().getColor(R.color.actionModeCallbackModel));
                 }
